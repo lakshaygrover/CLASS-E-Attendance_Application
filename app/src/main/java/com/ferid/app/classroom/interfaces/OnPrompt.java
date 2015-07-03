75c0d0b9e8a904a35b020ab1749c80177a3268ea
feridsource <ferid.developer@gmail.com>
2015-07-03 08:49:35 +0300

    Copyright
    Copyright 2015 Ferid Cafer

--- app/src/main/java/com/ferid/app/classroom/interfaces/OnPrompt.java
+++ app/src/main/java/com/ferid/app/classroom/interfaces/OnPrompt.java
@@ -1,8 +1,28 @@
+/*
+ * Copyright (C) 2015 Ferid Cafer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ferid.app.classroom.interfaces;
 
 /**
  * Created by ferid.cafer on 4/3/2015.
  */
 public interface OnPrompt {
+    /**
+     * On Prompt
+     * @param promptText String
+     */
     void OnPrompt(String promptText);
 }
