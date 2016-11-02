a6e1219417d840df23d09667929bbef60a84d9fe
ferid.cafer <ferid.developer@gmail.com>
2016-11-02 17:11:01 +0300

    v4.7.0
    v4.7.0

--- app/src/main/java/com/ferid/app/classroom/model/Attendance.java
+++ app/src/main/java/com/ferid/app/classroom/model/Attendance.java
@@ -16,13 +16,10 @@
 
 package com.ferid.app.classroom.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by ferid.cafer on 4/6/2015.
  */
-public class Attendance implements Parcelable {
+public class Attendance {
     private int id;
     private int classroomId;
     private int studentId;
@@ -88,38 +85,4 @@ public class Attendance implements Parcelable {
         this.studentName = studentName;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(id);
-        dest.writeInt(classroomId);
-        dest.writeInt(studentId);
-        dest.writeInt(present);
-        dest.writeString(dateTime);
-        dest.writeString(studentName);
-    }
-
-    public static final Parcelable.Creator<Attendance> CREATOR
-            = new Parcelable.Creator<Attendance>() {
-        public Attendance createFromParcel(Parcel in) {
-            return new Attendance(in);
-        }
-
-        public Attendance[] newArray(int size) {
-            return new Attendance[size];
-        }
-    };
-
-    protected Attendance(Parcel in) {
-        id = in.readInt();
-        classroomId = in.readInt();
-        studentId = in.readInt();
-        present = in.readInt();
-        dateTime = in.readString();
-        studentName = in.readString();
-    }
 }
